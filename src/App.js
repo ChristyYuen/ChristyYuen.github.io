2d3cd7e82c1dfea58f2d8cbe4b43c6f779c0684d
Christy Yuen <yu3nchr1sty@gmail.com>
2024-10-17 20:31:59 -0700

    Added a Test Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './components/pages/About';
 import Projects from './components/pages/Projects';
 import Resume from './components/pages/Resume';
+import Test from './components/pages/Test';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/about' component={About} />
           <Route path='/projects' component={Projects} />
           <Route path='/resume' component={Resume} />
+          <Route path='/test' component={Test} />
         </Switch>
       </Router>
     </>
